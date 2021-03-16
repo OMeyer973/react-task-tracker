e7f63972e5cea5613fca189e6f4599ac550e7eb4
Olivier <olivier.meyer973@gmail.com>
2021-03-16 20:51:30 -0300

    added routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
+
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       {/* <h1 style={HeadingStyle}>{title}</h1> */}
       <h1>{title}</h1>
-      <Button
-        color={showAddTask ? "red" : "green"}
-        text={showAddTask ? "close" : "add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAddTask ? "red" : "green"}
+          text={showAddTask ? "close" : "add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
